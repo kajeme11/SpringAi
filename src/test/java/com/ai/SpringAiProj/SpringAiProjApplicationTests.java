3cf1e74c5d506771b165cbd9b433fdd0c0891524
kajeme11 <chenequek@gmail.com>
2025-02-21 23:48:08 -0800

    chatService class, and controller

---
+++ src/test/java/com/ai/SpringAiProj/SpringAiProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.SpringAiProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
