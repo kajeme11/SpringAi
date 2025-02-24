f0776a20a495c3ad648e91cf86d105745069c70e
kajeme11 <chenequek@gmail.com>
2025-02-23 23:59:13 -0800

    openaI IMAGE MODEL

--- src/main/java/com/ai/SpringAiProj/GenAiController.java
+++ src/main/java/com/ai/SpringAiProj/GenAiController.java
@@ -1,16 +1,23 @@
 package com.ai.SpringAiProj;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class GenAiController {
 
-    ChatService chatService;
+    private final ChatService chatService;
+    private final ImageService imageService;
 
-    public GenAiController(ChatService chatService){
+    public GenAiController(ChatService chatService, ImageService imageService){
+
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
@@ -23,4 +30,10 @@ public class GenAiController {
         return chatService.getResponseOptions(prompt);
     }
 
+    @GetMapping("ask-ai-image")
+    public void generateImages(HttpServletResponse httpServletResponse,  @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl =  imageResponse.getResult().getOutput().getUrl();
+        httpServletResponse.sendRedirect(imageUrl);
+    }
 }
